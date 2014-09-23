c98e74587ca3c010fd78d7ba8f92b7323910ba3b
Es_Program <radwimps.september@gmail.com>
2014-09-23 21:03:41 +0900

    Chatの更新

--- Assets/Script/Chat.cs
+++ Assets/Script/Chat.cs
@@ -5,42 +5,58 @@ using UnityEngine;
 
 public class Chat : MonoBehaviour
 {
+    private enum NetworkState
+    {
+        None,
+        Server,
+        Client,
+    }
+
     private TCPServer server;
     private TCPClient client;
     private NetworkStream ns;
+    private NetworkState state = NetworkState.None;
 
     private void Update()
     {
-        //サーバの作業
-        if(server != null)
+        switch(state)
         {
-            if(server.Accepted)
-            {
-            }
-        }
+            case NetworkState.None:
+                break;
 
-        //クライアントの処理
-        if(client != null)
-        {
-            if(client.isConnected)
-            {
-            }
+            case NetworkState.Server:
+                if(server.Accepted) { }
+
+                break;
+
+            case NetworkState.Client:
+                if(client.isConnected) { }
+                break;
+
+            default:
+                break;
         }
     }
 
     private void OnGUI()
     {
-        //サーバになるボタン
-        if(GUI.Button(new Rect(10, 10, 150, 30), "サーバになる"))
+        //サーバ・クライアントが決定していない状態だったら
+        if(state == NetworkState.None)
         {
-            server = new TCPServer();
-            server.AcceptConnectAsync();
-        }
-        //クライアントになるボタン
-        if(GUI.Button(new Rect(10, 50, 150, 30), "クライアントになる"))
-        {
-            client = new TCPClient();
-            client.ConnectAsync(IPAddress.Loopback);
+            //サーバになるボタン表示
+            if(GUI.Button(new Rect(10, 10, 150, 30), "サーバになる"))
+            {
+                state = NetworkState.Server;
+                server = new TCPServer();
+                server.AcceptConnectAsync();
+            }
+            //クライアントになるボタン表示
+            if(GUI.Button(new Rect(10, 50, 150, 30), "クライアントになる"))
+            {
+                state = NetworkState.Client;
+                client = new TCPClient();
+                client.ConnectAsync(IPAddress.Loopback);
+            }
         }
     }
 }
